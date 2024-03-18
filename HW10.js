7edaefa2f03e0a160ba25e3558c892b321500278
OlhaKlochak <olga.klochak@gmail.com>
2024-03-18 19:17:34 +0200

    HW10

---
+++ HW10.js
@@ -0,0 +1,32 @@
+// Task 1
+var str='aaaaaa';
+var regEx=/^(?!.*[Аа]).{6,}$/;
+console.log(regEx.test(str));
+
+
+
+// Task 2
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" 
+    },
+]
+const emailRegex = /^[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*@(gmail\.com|yahoo\.com)$/;
+
+const trustedEmails = arr.filter(item => {
+    return emailRegex.test(item.email);
+});
+
+console.log(trustedEmails);
